1463206749ef786b26976c235555114d7ef81cef
Aziza-Azizova <aziza.azizova01@gmail.com>
2023-10-12 17:56:35 +0500

    Begining of the project, did some settings with firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,19 @@
+import Firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyDZEp1MpmXdmY6TkckQjnK_dw3nhh2UELw",
+    authDomain: "instagram-bc424.firebaseapp.com",
+    projectId: "instagram-bc424",
+    storageBucket: "instagram-bc424.appspot.com",
+    messagingSenderId: "998847646051",
+    appId: "1:998847646051:web:2db3febb1b5a2b2b3c6b56"
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+
+
+export {firebase, FieldValue};
